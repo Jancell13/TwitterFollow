a969a3c472348e5a4fcd8e0e2acf6d97a52c5ed2
Jancell13 <jancell642@gmail.com>
2023-07-28 15:07:08 -0500

    Primer Projecto React

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
+
+const users = [
+  {
+    userName: "jancell13",
+    name: "Jancell13",
+    isFollowing: true,
+  },
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "reddit",
+    name: "Reddit",
+    isFollowing: false,
+  },
+];
+
+export function App() {
+  return (
+    <section className="App">
+      <h2>A quién seguir</h2>
+
+      {users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  );
+}
